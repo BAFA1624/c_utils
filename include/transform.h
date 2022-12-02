5056da3da5c8145ede5aaad9e0c14b80adfe621b
Ben Andrews <benafandrews@gmail.com>
2022-12-02 14:47:45 +0000

    Gutted transform implementation, will require rework to match range interface changes.

--- include/transform.h
+++ include/transform.h
@@ -11,45 +11,11 @@
 
 struct ARGS_
 {
-    const size_t  n;
+    const size_t n;
     const void ** args;
 };
 
 
 typedef struct ARGS_ args;
-typedef range_t      tr_range_t;
-typedef rrange_t     trr_range_t;
-
-typedef void ( *transform_func )( const void **, void ** );
-typedef void ( *test_transform_func )( const void **, const args );
-
-
-tr_range_t  get_tr_range( void * const arr, const size_t, const size_t );
-trr_range_t get_trr_range( void * const arr, const size_t, const size_t );
-
-void apply_transform( void **, const size_t, const transform_func, ... );
-
-tr_range_t transform_range( const range_t, transform_func, const size_t );
-tr_range_t transform_const_range( const const_range_t, const transform_func,
-                                  const size_t );
-tr_range_t transform_tr_range( tr_range_t, const transform_func, const size_t );
-
-tr_range_t test_transform_range( const range_t, const test_transform_func,
-                                 const args );
-tr_range_t transform_arr( void * const, const size_t, const size_t,
-                          const transform_func, const size_t );
-
-
-// clang-format off
-
-#define transform( data, func, new_element_size )\
-    _Generic( (data),\
-            range_t: transform_range,\
-            const_range_t: transform_const_range,\
-            tr_range_t *: transform_tr_range,\
-            void*: transform_arr )\
-    ( ( data ), ( func ), ( new_element_size) )
-
-// clang-format on
 
 #endif
