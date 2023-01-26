5bd670a2b2d9fb1ceb17dc55b5c47057b41a8aa7
Ben Andrews <benafandrews@gmail.com>
2023-01-26 00:21:20 +0000

    Implemented len_cstr & find_cstr.

--- include/str.h
+++ include/str.h
@@ -1,6 +1,27 @@
 #ifndef STR_H
 #define STR_H
 
+#include <stdlib.h>
+
+size_t len_cstr( const char * const restrict s );
+
+// Returns a count of the target string found in the provided buffer.
+size_t find_cstr( const char * const buf, const size_t buf_sz,
+                  const char * const target, const size_t target_sz );
+
+// Returns the index positions of the target string found in the provided
+// buffer.
+size_t * find_pos_cstr( const char * const buf, const size_t buf_sz,
+                        const char * const target, size_t * n );
+
+size_t find_first_cstr( const char * const buf, const size_t buf_sz,
+                        const char * const target, const size_t target_sz );
+
+size_t replace_cstr( char * const s, const char * const target,
+                     const char * const replacement );
+
 int comp_cstr( const char * const lhs, const char * const rhs );
+
+char ** split_cstr( const char * buf, const size_t sz, const char * delim );
 
 #endif
--- src/str.c
+++ src/str.c
@@ -1,5 +1,14 @@
 #include "str.h"
 
+#include <stdio.h>
+
+size_t
+len_cstr( const char * restrict s ) {
+    const uintptr_t start = ( uintptr_t ) s;
+    while ( *s != '\0' ) s++;
+    return ( size_t ) ( ( uintptr_t ) s - start );
+}
+
 int
 comp_cstr( const char * const lhs, const char * const rhs ) {
     const char *lptr, *rptr;
@@ -13,4 +22,51 @@ comp_cstr( const char * const lhs, const char * const rhs ) {
 
     return 1;
 }
+
+size_t
+find_cstr( const char * const buf, const size_t buf_sz,
+           const char * const target, const size_t target_sz ) {
+    if ( buf == NULL || target == NULL )
+        return 0;
+    if ( buf_sz == 0 || target_sz == 0 )
+        return 0;
+
+    const char *l = buf, *r = l;
+    size_t      n = 0;
+
+    while ( *l != '\0' ) {
+    search:
+        if ( *l == target[0] ) {
+            size_t pos = 0;
+            while ( pos < target_sz ) {
+                if ( *r != target[pos++] ) {
+                    l = r;
+                    goto search;
+                }
+                r++;
+            }
+            r--;
+            l = r;
+            n++;
+        }
+        l++;
+        r++;
+    }
+
+    return n;
+}
+
+/*char **
+split_cstr( const char * const buf, const size_t sz, const char * delim ) {
+    const char *l = buf, *r = buf + sz - 1;
+
+    const size_t delim_len = len_cstr( delim );
+
+    // Count no. times delim appears in buf.
+    size_t n = 0;
+    do {
+    } while ( !l );
+
+    return NULL;
+}*/
 
